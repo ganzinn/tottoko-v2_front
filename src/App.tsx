d375ae94660f7bc25833aef3b13aa29b94c01365
ganzinn <ganzinnpg@gmail.com>
2022-02-24 20:11:59 +0900

    [ADD]全体背景色設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { VFC } from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 
+import { theme } from 'theme';
 import { Router } from 'router/Router';
 
-const App: VFC = () => <Router />;
+const App: VFC = () => (
+  <ChakraProvider theme={theme}>
+    <Router />
+  </ChakraProvider>
+);
 
 export default App;
