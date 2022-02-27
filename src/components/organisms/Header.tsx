7aecd5552ec74fb56b13b87dc03efdbe8ecbf95c
ganzinn <ganzinnpg@gmail.com>
2022-02-27 23:10:40 +0900

    [ADD]ヘッダー追加

---
+++ src/components/organisms/Header.tsx
@@ -0,0 +1,95 @@
+import { VFC } from 'react';
+import {
+  Flex,
+  Box,
+  Image,
+  Stack,
+  IconButton,
+  Spacer,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
+
+import logo from 'image/logo.png';
+
+type NavItem = {
+  label: string;
+  onClick: () => void;
+};
+type NavItems = {
+  topUrl: () => void;
+  navItems: NavItem[];
+};
+
+export const Header: VFC<NavItems> = ({ topUrl, navItems }) => (
+  <Flex
+    as="nav"
+    align="center"
+    justify="space-between"
+    wrap="wrap"
+    p={3}
+    bg="white"
+    shadow="sm"
+  >
+    <Box
+      onClick={topUrl}
+      fontSize={30}
+      color="orange.500"
+      letterSpacing="tighter"
+      fontWeight="semibold"
+      _hover={{ cursor: 'pointer' }}
+    >
+      <Image
+        src={logo}
+        display="inline"
+        height="2rem"
+        position="relative"
+        top="1"
+        mr="2px"
+      />
+      tottoko
+    </Box>
+
+    <Stack
+      direction="row"
+      display={{ base: 'none', md: 'flex' }}
+      width="auto"
+      alignItems="center"
+      flexGrow={1}
+    >
+      <Spacer />
+      {navItems.map(({ label, onClick }) => (
+        <Button
+          key={label}
+          onClick={onClick}
+          fontWeight="semibold"
+          bgColor="white"
+        >
+          {label}
+        </Button>
+      ))}
+    </Stack>
+
+    <Menu>
+      <MenuButton
+        as={IconButton}
+        icon={<HamburgerIcon boxSize={6} />}
+        display={{ base: 'block', md: 'none' }}
+        bgColor="white"
+      />
+      <MenuList display={{ base: 'block', md: 'none' }}>
+        {navItems.map(({ label, onClick }) => (
+          <MenuItem key={label} onClick={onClick} bgColor="white" mt={0}>
+            {label}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  </Flex>
+);
+
+export default Header;
--- src/containers/organisms/Header.tsx
+++ src/containers/organisms/Header.tsx
@@ -1,7 +1,47 @@
 import { VFC } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Header } from 'components/organisms/Header';
+import { useAppSelector } from 'store';
 
 export const EnhancedHeader: VFC = () => {
-  const pageName = 'EnhancedHeader';
+  const userAuth = useAppSelector((state) => state.userAuth);
+  const navigate = useNavigate();
+  const topUrl = () => navigate('/');
 
-  return <p>{pageName}</p>;
+  const linkItems = (() => {
+    if (userAuth) {
+      return [
+        {
+          label: `ログイン：${userAuth.loginUser.name}さん`,
+          onClick: () => navigate('/users/me'),
+        },
+        {
+          label: '作品投稿',
+          onClick: () => navigate('/users/me/works/entry'),
+        },
+        {
+          label: '家族設定',
+          onClick: () => navigate('/users/me/creators'),
+        },
+        {
+          label: 'ログアウト',
+          onClick: () => navigate('/users/sessions/logout'),
+        },
+      ];
+    }
+
+    return [
+      {
+        label: '新規登録',
+        onClick: () => navigate('/users/sign_up'),
+      },
+      {
+        label: 'ログイン',
+        onClick: () => navigate('/users/sessions/login'),
+      },
+    ];
+  })();
+
+  return <Header topUrl={topUrl} navItems={linkItems} />;
 };
