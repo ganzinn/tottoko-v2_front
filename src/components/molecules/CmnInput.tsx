8bf76c87b12bdcdcc25feec949e15a86687b3eba
ganzinn <ganzinnpg@gmail.com>
2022-03-08 09:31:00 +0900

    [ADD]ラベルフォントウエイト変更、任意項目ラベル追加

--- src/components/molecules/CmnInput.tsx
+++ src/components/molecules/CmnInput.tsx
@@ -1,13 +1,14 @@
-import { FormControl, FormLabel, forwardRef } from '@chakra-ui/react';
+import { FormControl, FormLabel, forwardRef, Text } from '@chakra-ui/react';
 import { MultipleFieldErrors } from 'react-hook-form';
 
 import { BaseInput, BaseInputProps } from 'components/atoms/BaseInput';
-import { EnhancedFormErrorMessageList } from 'containers/atoms/FormErrorMessageList';
+import { EnhancedFormErrorMessageArea } from 'containers/atoms/FormErrorMessageArea';
 
 export type CmnInputProps = BaseInputProps & {
   id?: string;
   labelName?: string;
   isRequired?: boolean;
+  optionalLabel?: boolean;
   isInvalid?: boolean;
   errorTypes?: MultipleFieldErrors;
 };
@@ -19,6 +20,7 @@ export const CmnInput = forwardRef<CmnInputProps, 'input'>(
       labelName,
       type = 'text',
       isRequired,
+      optionalLabel,
       isInvalid,
       errorTypes,
       ...rest
@@ -26,9 +28,16 @@ export const CmnInput = forwardRef<CmnInputProps, 'input'>(
     ref,
   ) => (
     <FormControl id={id} isRequired={isRequired} isInvalid={isInvalid}>
-      <FormLabel>{labelName}</FormLabel>
+      <FormLabel fontWeight="bold" mb={1} display="flex" alignItems="center">
+        <Text>{labelName}</Text>
+        {optionalLabel && (
+          <Text fontSize="xs" ml={1} p="2px" bgColor="gray.100">
+            任意
+          </Text>
+        )}
+      </FormLabel>
       <BaseInput ref={ref} type={type} {...rest} />
-      <EnhancedFormErrorMessageList errorTypes={errorTypes} />
+      <EnhancedFormErrorMessageArea errorTypes={errorTypes} />
     </FormControl>
   ),
 );
