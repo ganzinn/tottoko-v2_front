00374c75ca3a52700dc320a23b831a3b75baf453
ganzinn <ganzinnpg@gmail.com>
2022-03-15 03:27:23 +0900

    [ADD]react-queryプロバイダー設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { VFC } from 'react';
 import { Provider } from 'react-redux';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import { store } from 'store';
+import { queryClient } from 'queryClient';
 import { theme } from 'theme';
 import { Router } from 'router/Router';
 
 const App: VFC = () => (
   <Provider store={store}>
-    <ChakraProvider theme={theme}>
-      <Router />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Router />
+      </ChakraProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </Provider>
 );
 
