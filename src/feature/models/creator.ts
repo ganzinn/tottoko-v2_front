c84fef91e60f53091ed4c07df8529f21dd85465e
ganzinn <ganzinnpg@gmail.com>
2022-03-23 18:44:09 +0900

    [ADD]クリエーターモデル型追加

--- src/feature/models/creator.ts
+++ src/feature/models/creator.ts
@@ -1,10 +1,32 @@
 export type Creator = {
-  id: number;
+  id: string;
   name: string;
-  avatarUrl?: string;
   dateOfBirth: string;
   age: {
     years: number;
     months: number;
   };
+  avatarUrl?: string;
+};
+
+export type CreatorDetail = Omit<Creator, 'avatarUrl'> & {
+  gender?: {
+    id: string;
+    value: string;
+  };
+  originalAvatarUrl?: string;
+  editPermission: boolean;
+};
+
+export type CreatorFamily = {
+  id: string;
+  user: {
+    id: string;
+    name: string;
+  };
+  relation: {
+    id: string;
+    value: string;
+  };
+  familyRemovePermission: boolean;
 };
