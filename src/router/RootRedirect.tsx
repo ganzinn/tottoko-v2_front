036ed343c29d52f438f814839245de8aa466059c
ganzinn <ganzinnpg@gmail.com>
2022-04-13 23:47:40 +0900

    [FIX]ルートパス、リダイレクト先変更

---
+++ src/router/RootRedirect.tsx
@@ -0,0 +1,15 @@
+import { VFC } from 'react';
+import { useLocation, Navigate } from 'react-router-dom';
+
+import { isUserAuthSelector, useAppSelector } from 'store';
+
+export const RootRedirect: VFC = () => {
+  const isUserAuth = useAppSelector(isUserAuthSelector);
+  const location = useLocation();
+
+  if (!isUserAuth) {
+    return <Navigate to="/about" state={{ from: location }} />;
+  }
+
+  return <Navigate to="/users/me/works" />;
+};
